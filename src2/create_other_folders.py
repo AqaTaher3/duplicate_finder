e1b6cc98648fe6a6fbed8db2ab4cce2a0d2c8472
HP <aqa.taher3@gmail.com>
2025-10-19 17:05:07 +0330

    new gmail

--- src2/create_other_folders.py
+++ src2/create_other_folders.py
@@ -1,13 +1,25 @@
 import os
 
-
 def making_folders(folder_selected):
-    priority_folder = os.path.join(folder_selected, "000_PriorityFolder")
-    corrupted_folder = os.path.join(folder_selected, "000_corrupted_folder")
-    keep_folder = os.path.join(folder_selected, "000_keep_folder")
+    base_000 = os.path.join(folder_selected, "000")
+    priority_folder = os.path.join(base_000, "PriorityFolder")
+    corrupted_folder = os.path.join(base_000, "Corrupted_folder")
+    keep_folder = os.path.join(folder_selected, "000_Keep_folder")
 
-    os.makedirs(priority_folder, exist_ok=True)
-    os.makedirs(corrupted_folder, exist_ok=True)
-    os.makedirs(keep_folder, exist_ok=True)
+    # اگر 000 وجود ندارد، بساز
+    if not os.path.exists(base_000):
+        os.makedirs(base_000)
+
+    # اگر priority_folder وجود ندارد، بساز
+    if not os.path.exists(priority_folder):
+        os.makedirs(priority_folder)
+
+    # اگر corrupted_folder وجود ندارد، بساز
+    if not os.path.exists(corrupted_folder):
+        os.makedirs(corrupted_folder)
+
+    # اگر keep_folder وجود ندارد، بساز
+    if not os.path.exists(keep_folder):
+        os.makedirs(keep_folder)
 
     return [keep_folder, priority_folder, corrupted_folder]
